acf6132d6338143ab24fa87fb204d5ef1714f331
Mederic Roy Beldia <twocngdagz@yahoo.com>
2019-09-01 02:14:06 +0800

    bind Formbuilder to SimpleForm Html generator

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Html\Builder\FormBuilder;
+use App\Html\Builder\SimpleForm;
+use App\Html\HtmlFactory;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(FormBuilder::class, function () {
+            return new SimpleForm(new HtmlFactory);
+        });
     }
 }
