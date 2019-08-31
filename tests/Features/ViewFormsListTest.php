7c32152421fa43022a1f81a6713d9240ffb4771d
Mederic Roy Beldia <twocngdagz@yahoo.com>
2019-09-01 02:15:38 +0800

    add test for verifying that element is generated properly when form is shown

--- tests/Features/ViewFormsListTest.php
+++ tests/Features/ViewFormsListTest.php
@@ -37,14 +37,117 @@ class ViewFormsListTest extends TestCase
     /** @test */
     public function user_can_view_a_text_in_the_form()
     {
-        //Arrange
         $form = factory(Form::class)->states('published')->create();
 
         $form->questions()->saveMany([
             factory(Question::class)->make([
                 'name' => 'first_name',
                 'label' => 'What is you first Name',
-                'type' => 'textbox'
+                'type' => 'text'
+            ])
+        ]);
+
+        $this->visit("/forms/" . $form->id);
+
+        $this->seeElement("input", [
+            "name" => "first_name",
+            "type" => "text"
+        ]);
+
+        $this->seeElement("input", [
+            "type" => "submit"
+        ]);
+    }
+
+    /** @test */
+    public function user_can_view_a_textarea_in_the_form()
+    {
+        $form = factory(Form::class)->states('published')->create();
+
+        $form->questions()->saveMany([
+            factory(Question::class)->make([
+                'name' => 'description',
+                'label' => 'Description',
+                'type' => 'textarea'
+            ])
+        ]);
+
+        $this->visit("/forms/" . $form->id);
+
+        $this->seeElement("textarea", [
+            "name" => "description"
+        ]);
+
+        $this->seeElement("input", [
+            "type" => "submit"
+        ]);
+    }
+
+    /** @test */
+    public function user_can_view_a_input_type_radio_in_the_form()
+    {
+        $form = factory(Form::class)->states('published')->create();
+
+        $form->questions()->saveMany([
+            factory(Question::class)->make([
+                'name' => 'gender',
+                'label' => 'Gender',
+                'type' => 'radio',
+                'options' => [
+                    "male" => "Male",
+                    "female" => "Female",
+                    "other" => "Other"
+                ]
+            ])
+        ]);
+
+        $this->visit("/forms/" . $form->id);
+
+        $this->seeElement("input", [
+            "type" => "radio",
+            "name" => "gender"
+        ]);
+
+        $this->seeElement("input", [
+            "type" => "radio",
+            "value" => "male"
+        ]);
+
+        $this->seeElement("input", [
+            "type" => "radio",
+            "value" => "female"
+        ]);
+
+        $this->seeElement("input", [
+            "type" => "radio",
+            "value" => "other"
+        ]);
+
+        $this->seeElement("input", [
+            "type" => "submit"
+        ]);
+
+        $this->see("Male");
+        $this->see("Female");
+        $this->see("Other");
+    }
+
+    /** @test */
+    public function user_can_view_input_type_select_in_the_form()
+    {
+        //Arrange
+        $form = factory(Form::class)->states('published')->create();
+
+        $form->questions()->saveMany([
+            factory(Question::class)->make([
+                'name' => 'gender',
+                'label' => 'Gender',
+                'type' => 'select',
+                'options' => [
+                    "male" => "Male",
+                    "female" => "Female",
+                    "other" => "Other"
+                ]
             ])
         ]);
 
@@ -52,9 +155,28 @@ class ViewFormsListTest extends TestCase
         $this->visit("/forms/" . $form->id);
 
         //Assert
-        $this->seeElement("input", [
-            "name" => "first_name",
-            "type" => "text"
+        $this->seeElement("select", [
+            "name" => "gender"
         ]);
+
+        $this->seeElement("option", [
+            "value" => "male"
+        ]);
+
+        $this->seeElement("option", [
+            "value" => "female"
+        ]);
+
+        $this->seeElement("option", [
+            "value" => "other"
+        ]);
+
+        $this->seeElement("input", [
+            "type" => "submit"
+        ]);
+
+        $this->see("Male");
+        $this->see("Female");
+        $this->see("Other");
     }
 }
