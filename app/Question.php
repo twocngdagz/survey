9add2d35e8bd67cbb613c2c894f849ac6a60c79f
Mederic Roy Beldia <twocngdagz@yahoo.com>
2019-09-01 02:11:23 +0800

    add mutators for question model

--- app/Question.php
+++ app/Question.php
@@ -2,12 +2,24 @@
 
 namespace App;
 
+use App\Html\Builder\FormBuilder;
+use App\Html\HtmlFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    public function render()
+    public function render(FormBuilder $builder)
     {
-        return "<label>{$this->label}</label><input type=\"text\" name=\"{$this->name}\">";
+        return $builder->{$this->type}($this);
+    }
+
+    public function getOptionsAttribute($value)
+    {
+        return unserialize($value);
+    }
+
+    public function setOptionsAttribute($value)
+    {
+        $this->attributes['options'] = serialize($value);
     }
 }
